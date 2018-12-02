fac0cf1927d09cc1c1641f03b71e72b0ab7024fa
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2018-12-02 18:29:26 +0100

    Clean up go-solution

--- 2018/day-2/go/main.go
+++ 2018/day-2/go/main.go
@@ -1,44 +1,29 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
 	"strings"
 )
 
-func hasTwoThreeIdentical(s string) (bool, bool) {
-	charMap := make(map[rune]int)
-	for _, c := range s {
-		charMap[c]++
-	}
-	hasTwo := false
-	hasThree := false
-	for _, v := range charMap {
-		switch v {
-		case 2:
-			hasTwo = true
-		case 3:
-			hasThree = true
-		default:
-		}
-	}
-	return hasTwo, hasThree
-}
-
-func printCheckSum(filename string) error {
-	f, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
+func checksum(ids []string) int {
 	var countTwo, countThree int
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		hasTwo, hasThree := hasTwoThreeIdentical(scanner.Text())
+	for _, id := range ids {
+		charMap := make(map[rune]int)
+		for _, c := range id {
+			charMap[c]++
+		}
+		var hasTwo, hasThree bool
+		for _, v := range charMap {
+			switch v {
+			case 2:
+				hasTwo = true
+			case 3:
+				hasThree = true
+			default:
+			}
+		}
 
 		if hasTwo {
 			countTwo++
@@ -49,16 +34,10 @@ func printCheckSum(filename string) error {
 	}
 
 	checkSum := countTwo * countThree
-	fmt.Printf("- The checksum for boxes with twos (%d) and threes (%d) is: %d\n", countTwo, countThree, checkSum)
-	return nil
+	return checkSum
 }
 
-func findAlmostEquals(filename string) error {
-	bytes, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return err
-	}
-	ids := strings.Split(string(bytes), "\n")
+func commonLettersOfCorrectBoxes(ids []string) string {
 
 	// Compare all IDs to all other IDs
 	for i := 0; i < len(ids); i++ {
@@ -68,46 +47,45 @@ func findAlmostEquals(filename string) error {
 			id2 := ids[j]
 
 			//Compare ID 1 to ID 2
-			differencesFound := 0
+			commonChars := ""
 			for k := 0; k < len(id1); k++ {
-
-				if id1[k] != id2[k] {
-					differencesFound++
+				if id1[k] == id2[k] {
+					commonChars += string(id1[k])
 				}
 			}
 
-			if differencesFound == 1 {
-				fmt.Print("- First almost right id: ")
-				printIdenticalParts(id1, id2)
-				return nil
+			// IDs differ by only 1 character
+			if len(commonChars) == len(id1)-1 {
+				return commonChars
 			}
 		}
 	}
-	return nil
+	return ""
 }
 
-func printIdenticalParts(id1, id2 string) {
+func printIdenticalParts(id1, id2 string) string {
+	s := ""
 	for k := 0; k < len(id1); k++ {
 		if id1[k] == id2[k] {
-			fmt.Print(string(id1[k]))
+			s += string(id1[k])
 		}
 	}
+	return s
 }
 
 func main() {
 	fmt.Println("Advent of Code - Day 2 - Go")
 
+	bytes, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ids := strings.Split(string(bytes), "\n")
+
 	// Print checksum for part 1
 	fmt.Println("Part 1")
-	err := printCheckSum("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("- Checksum is:", checksum(ids))
 
 	fmt.Println("Part 2")
-	err = findAlmostEquals("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	fmt.Println("- Common characters of ID is:", commonLettersOfCorrectBoxes(ids))
 }
