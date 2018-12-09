6fb88e93fdf529d4b95219c20b3009699be1bc69
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2018-12-09 15:15:08 +0100

    2017 - Day 9 - Pt 2 - Using linked list

--- 2018/day-9/go/main.go
+++ 2018/day-9/go/main.go
@@ -46,15 +46,76 @@ func marble(nPlayers, maxScore int) int {
 	return max
 }
 
+type node struct {
+	prev, next int
+	val        int //For simplicity
+}
+
+func linkedMarbles(nPlayers, maxScore int) int {
+	marbles := make([]node, maxScore+10, maxScore+10)
+	current := &node{
+		0,
+		0,
+		0,
+	}
+	marbles = append(marbles, *current)
+
+	score := make(map[int]int)
+	p, m := 0, 1
+	for ; m <= maxScore; p, m = (p+1)%nPlayers, m+1 {
+
+		if m%23 == 0 {
+			// fmt.Print("Current is", current)
+			score[p] += m
+
+			for i := 0; i < 7; i++ {
+				current = &marbles[current.prev]
+			}
+			// current = (current - 7 + len(marbles)) % len(marbles)
+			score[p] += current.val
+
+			// Link past node
+			marbles[current.prev].next = current.next
+			marbles[current.next].prev = current.prev
+
+			// To make bugs easier to find :-)
+			current = &marbles[current.next]
+		} else {
+			prev := &marbles[current.next]
+			next := &marbles[prev.next]
+
+			// Insert between
+			prev.next = m
+			next.prev = m
+			marbles[m] = node{
+				prev: prev.val,
+				next: next.val,
+				val:  m,
+			}
+
+			current = &marbles[m]
+		}
+
+	}
+
+	max := -1
+	for _, v := range score {
+		if v > max {
+			max = v
+		}
+	}
+
+	return max
+}
+
 func main() {
 	defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
 	fmt.Println("Advent of Code - Day 9 - Go")
 
-	// fmt.Println("Test 1 -", marble(9, 25))            // 45 min
-	// fmt.Println("Test 1 -", marble(10, 1618) == 8317) // 45 min
-	// fmt.Println("Test 1 -", marble(17, 1104) == 2764) // 45 min
+	fmt.Println("Test 1 -", linkedMarbles(9, 25))                                     // 45 min
+	fmt.Println("Test 1 -", linkedMarbles(10, 1618), linkedMarbles(10, 1618) == 8317) // 45 min
+	fmt.Println("Test 1 -", linkedMarbles(17, 1104), linkedMarbles(17, 1104) == 2764) // 45 min
 
-	fmt.Println("Part 1 -", marble(478, 71240)) // 45 min'
-	// 30ms --> 200ms --> 1.13s
-	// 960ms --> 6.63s --> 25.48s
+	// fmt.Println("Part 1 -", marble(478, 71240*3)) // 45 min'
+	fmt.Println("Part 1 -", linkedMarbles(478, 71240*100)) // 45 min'
 }
