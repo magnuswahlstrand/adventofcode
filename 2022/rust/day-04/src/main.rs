09787eb36c3215fcae178cc9b90b06f5008ddbbb
Magnus <magnus@magnuss-mbp.lan>
2022-12-05 21:25:58 +0100

    2022: day 4-5 in Rust

--- 2022/rust/day-04/src/main.rs
+++ 2022/rust/day-04/src/main.rs
@@ -1,11 +1,12 @@
+extern crate simple_error;
 extern crate sscanf;
 
-use std::{fs, io, result};
+use std::{fs, io};
+use std::error::Error;
 use std::io::BufRead;
+use std::str::FromStr;
 
 #[derive(Debug)]
-#[derive(sscanf::FromScanf)]
-#[sscanf(format = "{s}-{e}")]
 struct Range {
     s: u32,
     e: u32,
@@ -22,8 +23,6 @@ impl Range {
 }
 
 #[derive(Debug)]
-#[derive(sscanf::FromScanf)]
-#[sscanf(format = "{one},{two}")]
 struct ElfPair {
     one: Range,
     two: Range,
@@ -33,9 +32,33 @@ impl ElfPair {
     fn self_contained(&self) -> bool {
         self.one.contained(&self.two) | self.two.contained(&self.one)
     }
+    fn overlaps(&self) -> bool {
+        self.one.overlaps(&self.two) | self.two.overlaps(&self.one)
+    }
 }
 
-fn part_1(filename: &str) -> result::Result<i32, io::Error> {
+impl FromStr for ElfPair {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (elf_a, elf_b) = s.split_once(",").unwrap();
+        // let (elf_a, elf_b) = s.split([',','-']).unwrap();
+        let (a_start, a_end) = elf_a.split_once("-").unwrap();
+        let (b_start, b_end) = elf_b.split_once("-").unwrap();
+        Ok(ElfPair {
+            one: Range {
+                s: a_start.parse::<u32>()?,
+                e: a_end.parse::<u32>()?,
+            },
+            two: Range {
+                s: b_start.parse::<u32>()?,
+                e: b_end.parse::<u32>()?,
+            },
+        })
+    }
+}
+/*
+fn part_1(filename: &str) -> Result<i32, io::Error> {
     let file = fs::File::open(filename)?;
     let reader = io::BufReader::new(file);
     let mut sum = 0;
@@ -49,7 +72,7 @@ fn part_1(filename: &str) -> result::Result<i32, io::Error> {
     Ok(sum)
 }
 
-fn part_2(filename: &str) -> result::Result<i32, io::Error> {
+fn part_2(filename: &str) -> Result<i32, io::Error> {
     let file = fs::File::open(filename)?;
     let reader = io::BufReader::new(file);
     let mut sum = 0;
@@ -62,10 +85,34 @@ fn part_2(filename: &str) -> result::Result<i32, io::Error> {
     }
     Ok(sum)
 }
+*/
+
+fn part_1_2(filename: &str) -> Result<i32, Box<dyn Error>> {
+    let file = fs::File::open(filename)?;
+    let reader = io::BufReader::new(file);
+    let sum: i32 = reader.lines()
+        .filter_map(|line_result| line_result.ok()) // #1
+        .filter_map(|line| line.parse().ok())
+        .map(|pair: ElfPair| if pair.self_contained() { 1 } else { 0 })
+        .sum();
+
+    Ok(sum)
+}
+
+fn part_2_2(filename: &str) -> Result<i32, Box<dyn Error>> {
+    let file = fs::File::open(filename)?;
+    let reader = io::BufReader::new(file);
+    let sum: i32 = reader.lines()
+        .filter_map(|line_result| line_result.ok()) // #1
+        .filter_map(|line| line.parse().ok())
+        .map(|pair: ElfPair| if pair.overlaps() { 1 } else { 0 })
+        .sum();
+
+    Ok(sum)
+}
 
 fn main() {
     let filename = "input.txt";
-    // let filename = "input_test.txt";
-    println!("Part 1: {:?}", part_1(filename).unwrap());
-    println!("Part 2: {:?}", part_2(filename).unwrap());
+    println!("Part 1: {:?}", part_1_2(filename).unwrap());
+    println!("Part 2: {:?}", part_2_2(filename).unwrap());
 }
---
+++ 2022/rust/day-05/src/main.rs
@@ -0,0 +1,127 @@
+use std::{fs, io};
+use std::error::Error;
+use std::io::BufRead;
+
+#[derive(Debug)]
+struct StacksContainer {
+    stacks: Vec<Vec<char>>,
+}
+
+impl StacksContainer {
+    fn new(mut lines: Vec<String>) -> StacksContainer {
+        lines.reverse();
+        let mut stacks: Vec<Vec<char>> = Vec::new();
+        let num_stacks = (&lines[0].len() + 1) / 4;
+        for _ in 0..num_stacks {
+            stacks.push(Vec::new());
+        }
+        for line in &lines {
+            for (i, c) in line.chars().skip(1).step_by(4).enumerate() {
+                if c != ' ' {
+                    stacks[i].push(c);
+                }
+            }
+        }
+
+        println!("{:?}", (lines[0].len() + 1) / 4);
+        StacksContainer {
+            stacks: stacks,
+        }
+    }
+
+    fn move_between(&mut self, n: usize, from: usize, to: usize) {
+        for _ in 0..n {
+            let c = self.stacks[from - 1].pop().unwrap();
+            self.stacks[to - 1].push(c);
+        }
+    }
+    fn move_between_v2(&mut self, n: usize, from: usize, to: usize) {
+        let range = self.stacks[from - 1].len() - n..;
+        let tmp: Vec<char> = self.stacks[from - 1].drain(range).collect();
+        self.stacks[to - 1].extend(tmp.iter());
+    }
+
+
+    fn final_words(&self) {
+        for c in &self.stacks {
+            print!("{}", c.last().unwrap());
+        }
+        println!("\n")
+    }
+}
+
+
+fn part_1(filename: &str) -> Result<&str, Box<dyn Error>> {
+    let file = fs::File::open(filename).unwrap();
+    let reader = io::BufReader::new(file);
+
+    let mut iterator = reader.lines();
+
+    // Build initial stacks
+    let initial: Vec<String> = iterator.by_ref()
+        .map(|line| line.unwrap())
+        .take_while(|line| line.trim().starts_with("["))
+        .collect();
+    let mut stacks = StacksContainer::new(initial.clone());
+
+    // Skip empty line
+    iterator.by_ref().next();
+
+    for line in iterator.by_ref() {
+        let line = line?;
+        let action = line.split(" ").collect::<Vec<&str>>();
+        if let [a, b, c] = [&action[1], &action[3], &action[5]] {
+            stacks.move_between(
+                a.parse::<usize>().unwrap(),
+                b.parse::<usize>().unwrap(),
+                c.parse::<usize>().unwrap(),
+            )
+        }
+    }
+
+    println!("{:?}", stacks);
+    stacks.final_words();
+
+    Ok("")
+}
+
+fn part_2(filename: &str) -> Result<&str, Box<dyn Error>> {
+    let file = fs::File::open(filename).unwrap();
+    let reader = io::BufReader::new(file);
+
+    let mut iterator = reader.lines();
+
+    // Build initial stacks
+    let initial: Vec<String> = iterator.by_ref()
+        .map(|line| line.unwrap())
+        .take_while(|line| line.trim().starts_with("["))
+        .collect();
+    let mut stacks = StacksContainer::new(initial.clone());
+
+    // Skip empty line
+    iterator.by_ref().next();
+
+    for line in iterator.by_ref() {
+        let line = line?;
+        let action = line.split(" ").collect::<Vec<&str>>();
+        if let [a, b, c] = [&action[1], &action[3], &action[5]] {
+            stacks.move_between_v2(
+                a.parse::<usize>().unwrap(),
+                b.parse::<usize>().unwrap(),
+                c.parse::<usize>().unwrap(),
+            )
+        }
+    }
+
+    println!("{:?}", stacks);
+    stacks.final_words();
+
+    Ok("")
+}
+
+fn main() {
+    let filename = "input.txt";
+    // let filename = "input_test.txt";
+    println!("Part 1: {:?}", part_1(filename).unwrap());
+    println!("Part 2: {:?}", part_2(filename).unwrap());
+}
